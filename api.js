85f0c59d726f0c4271363c34836d7b4c37653a3d
Gustavo-PGM <cavalcantegustavo683@gmail.com>
2026-02-27 09:57:47 -0300

    featura: Liturgia completa adicionada

--- api.js
+++ api.js
@@ -1,16 +1,19 @@
-import { Extrairhomilia} from "./scraping.js";
 import express from "express";
+import { extrairLiturgia, Extrairhomilia } from "./scraping.js"; 
+import 'dotenv/config';
 
 const app = express();
-const PORT = process.env.PORT || 333
-
 
 app.get("/", async (request, response) => {
 
-  const homilia = await Extrairhomilia()
+  // pega a liturgia e a homilia do dia simultaneamente
+  const [liturgia, homilia] = await Promise.all([
+    extrairLiturgia(),
+    Extrairhomilia()
+  ]);
   
-  response.json({homilia})
-})
-
+  // '...liturgia' já manda Leitura, Salmo e Evangelho aqui dentro direto
+  response.json({ ...liturgia, homilia });
+});
 
 export default app;
